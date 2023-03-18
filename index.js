0b1fc66987eb4b8f7bb13ca5d4dd07a16c0be649
JakubZegota <s24317@pjwstk.edu.pl>
2023-03-18 19:17:28 +0100

    Deleting and blocking subquestions, refactoring

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
+//When "Add a question" is clicked"
 const generateFormButton = document.getElementById('generate-form-btn');
-
 generateFormButton.addEventListener('click', function () {
     event.preventDefault();
     const formContainer = document.getElementById('forms');
-    createQuestion(false, formContainer);
+    createQuestion(false, formContainer, null);
 });
 
 
-
-function createQuestion(isSubquestion, container) {
-
+//Creating question
+function createQuestion(isSubquestion, container, motherValue) {
 
     //a container for the question
     const questionContainer = document.createElement('div');
@@ -18,20 +17,44 @@ function createQuestion(isSubquestion, container) {
 
     //creating form element
     const formElement = document.createElement('form');
+
+    //ensuring every form has ID value --- NOT COMPLETED ---
     const formId = `form-${Date.now()}`;
     formElement.setAttribute('id', formId);
 
     if (isSubquestion) {
         formElement.classList.add('simple-subform');
+
+        //blocking the previous question and its type so it cannot be modified
+        blockOrUnblock(container, true);
+
+        //What conditions to add based on the previously chosen input
+        switch (motherValue) {
+            case 'text':
+                generateTypeInput('Condition', formElement, ["equals"]);
+                generateTextInput('', formElement);
+                break;
+            case 'yes/no':
+                generateTypeInput("Condition", formElement, ["equals"]);
+                generateTypeInput("", formElement, ["yes", "no"]);
+                break;
+            case 'number':
+                generateTypeInput("Condition", formElement, ["equals", "greater than", "less than"]);
+                generateTextInput('', formElement);
+                break;
+            default:
+                console.log("error with inheritance");
+                break;
+        }
     } else {
         formElement.classList.add('simple-form');
     }
 
     //generate input for questions
-    generateQuestionInput(formElement);
+    generateTextInput('Question', formElement);
 
     //generate input with arguments as options to select
-    generateTypeInput("Type: ",formElement, ['text', 'number', 'yes/no']);
+    generateTypeInput("Type", formElement, ['text', 'number', 'yes/no']);
 
     //adding elements to containers
     questionContainer.appendChild(document.createElement('br')); //container
@@ -47,20 +70,37 @@ function createQuestion(isSubquestion, container) {
     addSubquestion.textContent = "Add subquestion";
     addSubquestion.addEventListener('click', function () {
         event.preventDefault();
-        createQuestion(true, subquestionsContainer);
+        const typeValue = formElement.querySelector('select[name="Type"]').value;
+        createQuestion(true, subquestionsContainer, typeValue); // CHANGE THIS NULL!!!
+
     });
     addSubquestion.classList.add('subquestion-btn');
     formElement.appendChild(addSubquestion);
 
+
+    //button for deleting subquestions
+    const deleteSubquestion = document.createElement('button');
+    deleteSubquestion.textContent = "Delete subquestion";
+    deleteSubquestion.addEventListener('click', function () {
+        event.preventDefault();
+        const subquestionContainer = deleteSubquestion.closest('.question-container');
+        subquestionContainer.remove();
+        blockOrUnblock(container, false);
+    });
+    deleteSubquestion.classList.add('subquestion-delete-btn');
+    formElement.appendChild(deleteSubquestion);
+
+
 }
 
 
+//Function that creates 'select' type input
 function generateTypeInput(h1name, container, argsArray) {
     const typeTag = document.createElement('h4');
     typeTag.textContent = h1name;
     container.appendChild(typeTag);
     const selectElement = document.createElement('select');
-    selectElement.setAttribute('name', 'type');
+    selectElement.setAttribute('name', h1name);
     argsArray.forEach(element => {
         const textType = document.createElement('option');
         textType.textContent = element;
@@ -71,16 +111,18 @@ function generateTypeInput(h1name, container, argsArray) {
     container.appendChild(selectElement);
 }
 
-function generateQuestionInput(container) {
+//Function that creates 'text' type input
+function generateTextInput(h1name, container) {
     const questionTag = document.createElement('h4');
-    questionTag.textContent = "Question:"
+    questionTag.textContent = h1name;
     container.appendChild(questionTag);
     const questionInput = document.createElement('input');
     questionInput.setAttribute('type', 'text');
-    questionInput.setAttribute('name', 'question');
+    questionInput.setAttribute('name', h1name);
     container.appendChild(questionInput);
     container.appendChild(document.createElement('br'));
 }
+
 
 //dATA to JSON  -- not completed !!! --
 const submitFormsBtn = document.getElementById('submit-forms-btn');
@@ -98,3 +140,20 @@ submitFormsBtn.addEventListener('click', function () {
     });
     console.log(JSON.stringify(data));
 });
+
+
+function blockOrUnblock(container, isBlocking) {
+    const motherQuestion = container.closest('.question-container');
+    const motherForm = motherQuestion.querySelector('form');
+    if (isBlocking) {
+        const motherTypeSelect = motherForm.querySelector('select[name="Type"]');
+        motherTypeSelect.setAttribute('disabled', 'disabled');
+        const motherQuestionInput = motherForm.querySelector('input[name="Question"]');
+        motherQuestionInput.setAttribute('disabled', 'disabled');
+    } else {
+        const motherTypeSelect = motherForm.querySelector('select[name="Type"]');
+        motherTypeSelect.removeAttribute('disabled');
+        const motherQuestionInput = motherForm.querySelector('input[name="Question"]');
+        motherQuestionInput.removeAttribute('disabled');
+    }
+}
